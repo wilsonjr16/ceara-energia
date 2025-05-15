65b93fbd31692a21fba4824a61e015ac3186b2d7
wilson alves <wilson.alves@svm.com.br>
2025-05-15 13:07:20 -0300

    owl carousel

--- js/main.js
+++ js/main.js
@@ -48,5 +48,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+  });
+});
+
+
 
 
